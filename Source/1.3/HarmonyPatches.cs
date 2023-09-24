cfad91521455e0336434b6da0cf36a4b64125b61
Katana Crimson <121707+katanacrimson@users.noreply.github.com>
2023-09-24 18:45:53 +0000

    [ci] Automated git mirroring from cr.imson.co repository

---
+++ Source/1.3/HarmonyPatches.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace DisableRoofCheck
+{
+    [StaticConstructorOnStartup]
+    internal class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("katana.disableroofcheck");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Message("[DisableRoofCheck] Loaded");
+        }
+
+        [HarmonyPatch(typeof(RimWorld.Need_Indoors), "NeedInterval")]
+        static class Patch_Need_Indoors
+        {
+            static readonly float Delta_Indoors_ThickRoof = 2f;
+            static readonly float Delta_Indoors_NoRoof = 0f;
+            static readonly float Delta_Outdoors_ThickRoof = 0f;
+            static readonly float Delta_Outdoors_NoRoof = -0.25f;
+
+            [HarmonyPrefix]
+            static bool Prefix(Need_Indoors __instance, Pawn ___pawn, NeedDef ___def, ref float ___lastEffectiveDelta)
+            {
+                bool disabled = Traverse.Create(__instance).Property("Disabled").GetValue<bool>();
+                bool isFrozen = Traverse.Create(__instance).Property("IsFrozen").GetValue<bool>();
+
+                if (disabled)
+                {
+                    __instance.CurLevel = 1f;
+                }
+                else if (!isFrozen)
+                {
+                    float num = 0f;
+                    bool flag = !___pawn.Spawned || ___pawn.Position.UsesOutdoorTemperature(___pawn.Map);
+                    RoofDef roofDef = (___pawn.Spawned ? ___pawn.Position.GetRoof(___pawn.Map) : null);
+                    float curLevel = __instance.CurLevel;
+
+                    if (!flag)
+                    {
+                        num = (roofDef == null) ? Patch_Need_Indoors.Delta_Indoors_NoRoof : Patch_Need_Indoors.Delta_Indoors_ThickRoof;
+                    }
+                    else
+                    {
+                        num = (roofDef == null) ? Patch_Need_Indoors.Delta_Outdoors_NoRoof : Patch_Need_Indoors.Delta_Outdoors_ThickRoof;
+                    }
+
+                    num *= 0.0025f;
+                    if (num < 0f)
+                    {
+                        __instance.CurLevel = Mathf.Min(__instance.CurLevel, __instance.CurLevel + num);
+                    }
+                    else
+                    {
+                        __instance.CurLevel = Mathf.Min(__instance.CurLevel + num, 1f);
+                    }
+                    ___lastEffectiveDelta = __instance.CurLevel - curLevel;
+                }
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(ThoughtWorker_IsIndoorsForUndergrounder), "IsAwakeAndIndoors")]
+        public class ThoughtWorker_IsIndoorsForUndergrounderPatch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(ref bool __result, Pawn p, out bool isNaturalRoof)
+            {
+                isNaturalRoof = true;
+                __result = (p.Awake() && !(p.Position.UsesOutdoorTemperature(p.Map) || !p.Position.Roofed(p.Map)));
+                return false;
+            }
+        }
+    }
+}
---
+++ changelog.md
@@ -0,0 +1,15 @@
+## DisableRoofCheck changelog
+
+#### v1.0.9
+
+- restructured repository
+- update dependency versions, recompiled
+- published to github (sorry, builds still via gitlab for now)
+
+#### v1.0.8
+
+- rimworld 1.4 support
+
+#### v1.0.7
+
+- update dependency versions and recompile
